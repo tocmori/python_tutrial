84e9d917e2a5c13237aee2222fd296a0b619d505
Kenta Omori <omori.kenta@gmail.com>
2015-06-26 07:28:53 +0900

    Add hello logs

--- hello/views.py
+++ hello/views.py
@@ -3,12 +3,26 @@ from django.http import HttpResponse
 
 from .models import Greeting
 
+from logging import getLogger,StreamHandler,DEBUG
+logger = getLogger(__name__)
+handler = StreamHandler()
+handler.setLevel(DEBUG)
+logger.setLevel(DEBUG)
+logger.addHandler(handler)
+
+logger.debug('HelloViews')
+
 # Create your views here.
 def index(request):
+
+    logger.debug('index request')
+
     return HttpResponse('Hello from Python!')
 
 
 def db(request):
+
+    logger.debug('db request')
 
     greeting = Greeting()
     greeting.save()
